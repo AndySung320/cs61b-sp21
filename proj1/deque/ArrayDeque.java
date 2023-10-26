e4f28de1b65666fbf3b748d24a4944994bfa9267
AndySung0320 <andysung0320@gmail.com>
2023-10-26 18:50:18 +0800

    done with proj1-A

---
+++ proj1/deque/ArrayDeque.java
@@ -0,0 +1,199 @@
+package deque;
+import java.lang.Math;
+import java.util.Iterator;
+
+public class ArrayDeque<AnyType> implements Deque<AnyType>, Iterable<AnyType>{
+    private int nextFirst;
+    private int nextLast;
+    private AnyType items[];
+    private int size;
+
+    private int capacity = 8;
+
+    public ArrayDeque(){
+        items = (AnyType[]) new Object[4];
+        nextFirst = 0;
+        nextLast = 1;
+        size = 0;
+    }
+
+    /** Calculate the nextLast index. */
+    public int plusOne(int index){
+        return Math.floorMod(index + 1, items.length);
+    }
+
+    /** Calculate the nextFirst index. */
+    public int minusOne(int index){
+        return Math.floorMod(index - 1, items.length);
+    }
+
+    public AnyType get(int index){
+        if (index < 0 || index > size - 1 || isEmpty()){
+            return null;
+        }
+        int pos = Math.floorMod(plusOne(nextFirst) + index, items.length);
+        return items[pos];
+    }
+
+    /** Adds item to the front of the list. */
+    public void addFirst(AnyType item){
+        resize();
+        items[nextFirst] = item;
+        size = size + 1;
+        nextFirst = minusOne(nextFirst);
+    }
+
+    /** Adds item to the end of the list. */
+    public void addLast(AnyType item){
+        resize();
+        items[nextLast] = item;
+        nextLast = plusOne(nextLast);;
+        size = size + 1;
+    }
+    /** Remove the first element of the array. */
+    public AnyType removeFirst(){
+        if (this.isEmpty()){
+            return null;
+        }
+        resize();
+        int tmp = plusOne(nextFirst);
+        AnyType x = items[tmp];
+        items[tmp] = null;
+        nextFirst = tmp;
+        size = size - 1;
+        return x;
+    }
+
+    /** Remove the last element of the array. */
+    public AnyType removeLast(){
+        if (this.isEmpty()){
+            return null;
+        }
+        resize();
+        int tmp = minusOne(nextLast);//Math.floorMod(nextLast - 1 , items.length);
+        AnyType x = items[tmp];
+        items[tmp] = null;
+        nextLast = tmp;
+        size = size - 1;
+        return x;
+    }
+
+    public int size(){ return size; }
+
+    public boolean isEmpty(){
+        if (size == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public void resize(){
+        if (size == items.length){
+            expand();
+        }
+        if (size < (items.length * 0.25) && items.length > 8 ){
+            shrink();
+        }
+    }
+
+    public void expand(){
+        capacity = items.length * 2;
+        AnyType tmp [] = (AnyType[]) new Object[capacity];
+        int start = plusOne(nextFirst);
+        int end = minusOne(nextLast);
+        for (int i = 0; i < items.length; i++){
+            tmp[i] = items[start];
+            start = plusOne(start);
+        }
+        nextFirst = capacity - 1;
+        nextLast = items.length;
+        items = tmp;
+
+    }
+
+    public void shrink(){
+        capacity = items.length / 2;
+        AnyType tmp [] = (AnyType[]) new Object[capacity];
+        int start = plusOne(nextFirst);
+        int end = minusOne(nextLast);
+        for (int i = 0; i < items.length; i++){
+            tmp[i] = items[start];
+            start = plusOne(start);
+        }
+        nextFirst = capacity - 1;
+        nextLast = items.length;
+        items = tmp;
+
+    }
+
+    public void printDeque(){
+        int start = plusOne(nextFirst);
+        int end = nextLast;
+        for (int i = start; i != end; i = plusOne(i)) {
+            AnyType x = get(i);
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+
+    public boolean equals(Object o) {
+        if (o instanceof ArrayDeque oad) {
+            if (oad.size != this.size) {
+                return false;
+            }
+            int start = plusOne(nextFirst);
+            int end = nextLast;
+            for (int i = 0; i != end; i = plusOne(i)) {
+                if (this.get(i) != oad.get(i)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public Iterator<AnyType> iterator() {
+        return new ArrayDequeIterator();
+    }
+    public class ArrayDequeIterator implements Iterator<AnyType>{
+        private int wizPos;
+
+        private ArrayDequeIterator(){
+            wizPos = plusOne(nextFirst);
+        }
+
+        public boolean hasNext(){
+            return wizPos < nextLast;
+        }
+
+        public AnyType next(){
+            AnyType item = get(wizPos);
+            wizPos = plusOne(wizPos);
+            return item;
+        }
+
+
+    }
+    public static void main(String[] args){
+        ArrayDeque<Integer> A = new ArrayDeque();
+        A.addLast(1);
+        A.addLast(2);
+        A.addLast(3);
+        A.addFirst(4);
+        A.addLast(5);
+        A.printDeque();
+        System.out.printf("done\n");
+        Iterator<Integer> seer = A.iterator();
+        while(seer.hasNext()){
+            int i = seer.next();
+            System.out.println(i);
+        }
+
+    }
+
+}
+
+
---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,206 @@
+package deque;
+
+import java.util.ArrayDeque;
+import java.util.Iterator;
+
+import net.sf.saxon.om.Item;
+
+public class LinkedListDeque<AnyType> implements Deque<AnyType>, Iterable<AnyType>{
+
+    public class ItemNode{
+        public ItemNode prev;
+        public AnyType item;
+        public ItemNode next;
+        public ItemNode(AnyType x, ItemNode p, ItemNode n){
+            prev = p;
+            item = x;
+            next = n;
+        }
+    }
+    private ItemNode sentinel;
+    private int size;
+
+    public LinkedListDeque(){
+        size = 0;
+        sentinel = new ItemNode(null, null, null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+
+    }
+    public LinkedListDeque(AnyType item) {
+        size = 1;
+        sentinel = new ItemNode(null, null, null);
+        sentinel.next = new ItemNode(item, sentinel, sentinel);
+        sentinel.prev = sentinel.next;
+    }
+
+
+    /** Adds item to the front of the list. */
+    public void addFirst(AnyType item){
+        ItemNode tmp = new ItemNode(item, null,null);
+        tmp.next = sentinel.next;
+        tmp.prev = sentinel;
+        sentinel.next.prev = tmp;
+        sentinel.next = tmp;
+        size = size + 1;
+    }
+    /** Adds item to the end of the list. */
+    public void addLast(AnyType item){
+        ItemNode tmp = new ItemNode(item, null,null);
+        tmp.next = sentinel;
+        tmp.prev = sentinel.prev;
+        sentinel.prev.next = tmp;
+        sentinel.prev = tmp;
+        size = size + 1;
+    }
+    /** Returns if the list is empty. */
+    public boolean isEmpty(){
+        if (size == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    /** Returns the size of the list. */
+    public int size(){ return size; }
+
+    /** Print All the elements of the list. */
+    public void printDeque(){
+        ItemNode s = sentinel;
+        for (int i = 0; i < size; i++){
+            System.out.print(s.next.item + " ");
+            s = s.next;
+        }
+        System.out.println();
+    }
+
+    /** Remove the first element of the list. */
+    public AnyType removeFirst(){
+        if (size == 0){
+            return null;
+        }
+        AnyType x = sentinel.next.item;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
+        size = size - 1;
+        return x;
+    }
+
+    /** Remove the last element of the list. */
+    public AnyType removeLast(){
+        if (size == 0){
+            return null;
+        }
+        AnyType x = sentinel.prev.item;
+        sentinel.prev.prev.next = sentinel;
+        sentinel.prev = sentinel.prev.prev;
+        size = size - 1;
+        return x;
+    }
+    /** Get the element with index. */
+    public AnyType get(int index){
+        if (size == 0){
+            return null;
+        }
+        if (index >= 0 && index < size) {
+            ItemNode s = sentinel;
+            AnyType x = s.next.item;
+            for (int i = 0; i < index + 1; i++){
+                if (i == index){
+                    x = s.next.item;
+                }
+                s = s.next;
+            }
+            return x;
+        }
+        else {
+            return null;
+        }
+    }
+    /** HELPER - Get the element with index (Recursive). */
+    public AnyType helper(int index, ItemNode s){
+        if (index == 0){
+            return s.item;
+        }
+        else{
+            return helper(index - 1, s.next);
+        }
+    }
+    /** Get the element with index (Recursive). */
+    public AnyType getRecursive(int index){
+        if (size == 0 || index < 0 || index >= size){
+            return null;
+        }
+        if (index == 0){
+            return sentinel.next.item;
+        }
+        else{
+            return helper(index, sentinel.next);
+        }
+    }
+
+
+    public boolean equals(Object o){
+        if (o instanceof LinkedListDeque olld){
+            if (olld.size != this.size){
+                return false;
+            }
+            for (int i = 0; i < size; i++){
+                if (this.get(i) != olld.get(i)){
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+    public Iterator<AnyType> iterator() {
+        return new LinkedListIterator();
+    }
+    public class LinkedListIterator implements Iterator<AnyType>{
+        private int wizPos;
+        private LinkedListIterator(){
+            wizPos = 0;
+        }
+
+        public boolean hasNext(){
+            return wizPos < size;
+        }
+
+        public AnyType next(){
+            AnyType item = get(wizPos);
+            wizPos += 1;
+            return  item;
+        }
+
+
+    }
+
+
+
+
+
+
+    public static void main(String[] args){
+        LinkedListDeque L = new LinkedListDeque();
+        L.addFirst(4);
+        L.addFirst(3);
+        L.addFirst(2);
+        L.addFirst(1);
+        L.printDeque();
+
+        Iterator<Integer> seer = L.iterator();
+        while(seer.hasNext()){
+            int i = seer.next();
+            System.out.println(i);
+        }
+
+        //System.out.println(L.getRecursive(2));
+
+    }
+
+
+
+
+}
